22eac30756aa99fa686b675afa56fc167ea817f7
Omotolani Olurotimi <omotolaniolurotimi@gmail.com>
2024-03-18 13:32:34 +0100

    Signup Change

--- app/Http/Requests/StoreUserRequest.php
+++ app/Http/Requests/StoreUserRequest.php
@@ -23,8 +23,7 @@ class StoreUserRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'first_name' => 'required|string',
-            'last_name' => 'required|string',
+            'name' => 'required|string',
             'email' => 'required|string|email',
             'password' => ['required', 'string', Password::min(8)->mixedCase()->symbols()->uncompromised()]
         ];
